c71c6070ac99c46772616931e9f9ff5c8d892915
Sim Brar <sim.brar@hootsuite.com>
2017-02-21 22:59:19 -0800

    add entrylist to search bar and click through

--- src/components/EntrySearch.js
+++ src/components/EntrySearch.js
@@ -1,38 +1,68 @@
 import React, { Component } from 'react';
+import EntryList from './EntryList';
 
 export default class EntrySearch extends Component {
   constructor() {
     super();
 
+    this.renderEntryList = this.renderEntryList.bind(this);
+    this.onSubmit = this.onSubmit.bind(this);
+    this.onInputChange = this.onInputChange.bind(this);
+
     this.state = {
-        searchTerm: ''
+      searchTerm: '',
+      showEntryList: false
     }
   }
 
   onInputChange(e) {
-    this.setState({ searchTerm: event.target.value });
+    this.setState({ searchTerm: e.target.value });
   }
 
   onSubmit(e) {
     e.preventDefault();
 
-    this.props.onSubmit(this.state.searchTerm);
-    this.setState({ searchTerm: '' });
+    this.onSearch(this.state.searchTerm);
+    this.setState({ searchTerm: '', showEntryList: true });
+  }
+
+  onSearch(searchTerm) {
+    console.log("filter me");
+  }
+
+  renderEntryList() {
+    if (this.state.showEntryList) {
+      return(
+        <EntryList
+          entries={this.props.entries}
+          tags={this.props.tags}
+          onEntryClick={this.props.onEntryClick}
+        />
+      );
+    } else {
+      return;
+    }
   }
 
   render() {
     return(
-      <form onSubmit={this.onSubmit} className="input-group" id="entry-search">
-        <input
-          placeholder="Enter search text"
-          className="form-control"
-          value={this.state.searchTerm}
-          onChange={this.onInputChange}
-        />
-        <span className="input-group-btn">
-          <button type="submit" className="btn btn-search">Search Entries</button>
-        </span>
-      </form>
+      <div id="entry-search">
+        <form onSubmit={this.onSubmit} className="input-group">
+          <input
+            placeholder="Enter search text"
+            className="form-control"
+            value={this.state.searchTerm}
+            onChange={this.onInputChange}
+          />
+          <span className="input-group-btn">
+            <button type="submit" className="btn btn-search">Search Entries</button>
+          </span>
+        </form>
+        <div className="search-results">
+          <h4>Search results:</h4>
+          {this.renderEntryList()}
+        </div>
+      </div>
     );
   }
 }
