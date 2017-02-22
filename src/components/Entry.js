825131139c79c16ebff3eeafe860d3bdb34f87b0
Sim Brar <sim.brar@hootsuite.com>
2017-02-21 21:40:20 -0800

    add styles for entry list

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -2,18 +2,27 @@ import React, { Component } from 'react';
 import Tag from './Tag';
 
 export default class Entry extends Component {
+  truncateDate(date) {
+    return date.substring(0, date.indexOf('T'));
+  }
+
   render(){
     return(
-      <div className='entry-item-container'>
-        <div className='entry-text'>
-          {this.props.entry.entryText}
+      <div className="entry-item-container">
+        <div className="entry-header">
+          <div className="entry-date">
+            {this.truncateDate(this.props.entry.date)}
+          </div>
+          <div className="tag-container">
+            {
+              this.props.tags.map((tag) =>
+                <Tag key={tag.id} data={tag} />
+              )
+            }
+          </div>
         </div>
-        <div className='tag-container'>
-          {
-            this.props.tags.map((tag) =>
-              <Tag key={tag.id} data={tag} />
-            )
-          }
+        <div className="entry-text">
+          {this.props.entry.entryText}
         </div>
       </div>
     );
