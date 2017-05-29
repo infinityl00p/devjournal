9edc2f5f181d8c0829a20a62c9964f1b2d617ab2
james gill <infinityl00p>
2017-05-28 19:23:23 -0700

    Added loading animation

--- src/components/JournalPage/JournalPage.js
+++ src/components/JournalPage/JournalPage.js
@@ -112,10 +112,22 @@ class JournalPage extends Component {
   }
 
   render() {
-    // TODO: Make this UX better.
     if (!this.props.journal) {
       return(
-        <h1>Loading...</h1>
+        <div className="sk-circle">
+          <div className="sk-circle1 sk-child"></div>
+          <div className="sk-circle2 sk-child"></div>
+          <div className="sk-circle3 sk-child"></div>
+          <div className="sk-circle4 sk-child"></div>
+          <div className="sk-circle5 sk-child"></div>
+          <div className="sk-circle6 sk-child"></div>
+          <div className="sk-circle7 sk-child"></div>
+          <div className="sk-circle8 sk-child"></div>
+          <div className="sk-circle9 sk-child"></div>
+          <div className="sk-circle10 sk-child"></div>
+          <div className="sk-circle11 sk-child"></div>
+          <div className="sk-circle12 sk-child"></div>
+        </div>
     )}
     return(
       <div id="journal-page-container">
